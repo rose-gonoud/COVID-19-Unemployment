61c8f29542e7c7b6c7631d205fd10bf3c20d0230
Luisa zini <peldenchodron@gmail.com>
2020-04-18 18:09:08 -0700

    updated populatePulldown

--- assets/js/populatePulldown.js
+++ assets/js/populatePulldown.js
@@ -1,36 +1,48 @@
 // State selection
-states=[];
 
-// read the data
-stateData.forEach((s) => {
-  states.push(s.state);
-});
-console.log(states);
+console.log(stateData);
 
-
-pullDownMenu(states);
+pullDownMenu();
 
 /**
  * 
  * @param {var} value. This function creates array of options using the array of values in @param 
  */
 
-function pullDownMenu(value) {
+function pullDownMenu() {
 
   var dropdown = d3.select("#selState");
   // Log the entire dataset
-  console.log(value);
-
 
     // For each ID in the array run a function
-  value.forEach((element) => {
-      console.log(element);
-      // Append an option element to the #selDataset dropdown with the id
-      // in the value attribute as well as text between the open and closed tags.
-      dropdown.append('option')
-          .attr("value", element)
-          .text(element);
-  });
+    stateData.forEach((element) => {
+        console.log(element);
+        // Append an option element to the #selDataset dropdown with the id
+        // in the value attribute as well as text between the open and closed tags.
+        dropdown.append('option')
+            .attr("value", element.abbr)
+            .text(element.state);
+    });  
 
 };
-  
+
+/**
+ * When the select dropdown is changed this function will fire
+ * @param {Number} selectedValue This is the selected id in the pulldow from #selDataset.
+ */
+
+function optionChanged() {
+    /**
+     * collect selected values
+     * 
+     */
+      var selValues = [];
+      selValues.push($('#selState').val()); 
+      console.log(selValues);
+      d3.select("#h-pulldown").text(selValues);
+      
+              
+      console.log(`You changed the value. It is now: ${selValues}`);
+     
+};
+        
