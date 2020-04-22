eb02e9735265524436daaeb108a5bac853cbc2b2
Jesse Caldwell <CollectionOfAtoms@gmail.com>
2020-04-21 19:40:30 -0700

    Basic SummaryStats model

--- assets/js/summarystats.js
+++ assets/js/summarystats.js
@@ -1,8 +1,36 @@
-var apiReturn = [];
+function populateSummaryStats(data) {
+  console.log("data in summarystats", data);
 
+  d3.select("#summaryStats").text(" ");
 
-console.log(apiReturn);
+  let stats = calculateStats(data);
 
+  for (let [key, value] of Object.entries(stats)) {
+    d3.select("#summaryStats").append("div").text(`${key}: ${value}`);
+  }
+}
+
+// Takes in a data set and returns an object of relevant statistics
+function calculateStats(data) {
+  stats = {};
+
+  stats["State with most continued claims"] = getStateWithMaxContClaims(data);
+
+  return stats;
+}
+
+function getStateWithMaxContClaims(data) {
+  //Get the state with the most open claims within the period.
+  let allContinuedClaims = data.map((entry) => {
+    return entry.continued_claims;
+  });
+
+  let maxContinuedClaim = Math.max(...allContinuedClaims);
+
+  let maxContinuedClaimsIndex = allContinuedClaims.indexOf(maxContinuedClaim);
+
+  return data[maxContinuedClaimsIndex].state;
+}
 
 // d3.select("#summarystats")
 //     .selectAll("div").remove();
@@ -12,8 +40,6 @@ console.log(apiReturn);
 //         .append("div").text(`${key}: ${value}`);
 //       };
 
-
-
 //   .data(apiReturn)
 //   .enter()
 //   .append("tr")
@@ -21,13 +47,11 @@ console.log(apiReturn);
 //     return `<td>${d.date}</td><td>${d.low}</td><td>${d.high}</td>`;
 //   });
 
-
-
-  // continued_claims: 22085
-  // covered_employment: 1894608
-  // file_week_ended: "Sat, 05 Jan 2019 00:00:00 GMT"
-  // initial_claims: 6660
-  // insured_unemployment_rate: 1.17
-  // reflecting_week_ended: "Sat, 29 Dec 2018 00:00:00 GMT"
-  // state: "Alabama"
-  // state_abbr: "AL"
+// continued_claims: 22085
+// covered_employment: 1894608
+// file_week_ended: "Sat, 05 Jan 2019 00:00:00 GMT"
+// initial_claims: 6660
+// insured_unemployment_rate: 1.17
+// reflecting_week_ended: "Sat, 29 Dec 2018 00:00:00 GMT"
+// state: "Alabama"
+// state_abbr: "AL"
