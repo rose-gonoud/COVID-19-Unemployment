a0b0a5cce060a13220a414f15b0e440c06390c2f
Rose <rose.gonoud@gmail.com>
2020-04-25 01:22:05 -0700

    added legend info for covid data layer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,7 +6,7 @@ var myMap = L.map("map", {
 addLayers(myMap);
 addLegend(myMap);
 
-// global to hold the chloropleth, so we can remove it
+// global to hold the choropleth, so we can remove it
 var geojson;
 
 //Adds street, light and dark layers to the map
@@ -56,89 +56,16 @@ function addLayers(myMap) {
   //Add controls to switch between layers
   L.control.layers(baseMaps).addTo(myMap);
 }
-//Given a value returns a color code
-function getColor(d) {
-  return d > 100000
-    ? "#008000"
-    : d > 50000
-    ? "#3d9200"
-    : d > 20000
-    ? "#61a400"
-    : d > 10000
-    ? "#81b600"
-    : d > 5000
-    ? "#a1c800"
-    : d > 2000
-    ? "#c0da00"
-    : d > 1000
-    ? "#dfed00"
-    : d > 1
-    ? "ffff00"
-    : (d = 0 ? "black" : "black");
-}
 
-//Add the legend
-function addLegend(myMap) {
-  var legend = L.control({ position: "bottomright" });
-
-  legend.onAdd = function (map) {
-    var div = L.DomUtil.create("div", "info legend"),
-      grades = [0, 1, 1000, 2000, 5000, 10000, 20000, 50000, 100000],
-      labels = [];
-
-    //Add special label for no data
-    div.innerHTML +=
-      '<i style="background:' + getColor(grades[0] + 1) + '"></i> No Data <br>';
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 1; i < grades.length; i++) {
-      div.innerHTML +=
-        '<i style="background:' +
-        getColor(grades[i] + 1) +
-        '"></i> ' +
-        grades[i] +
-        (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-    }
-
-    return div;
-  };
-
-  legend.addTo(myMap);
-}
-
-/**
- * Takes in geoJson object and appends data returned from the API to each states geoJSON entry
- * @param {geoJSON OBJECT} geoData The geoJson that is returned from d3.json when querying the geojson file attached
- * @param {*} apiReturn The return from the API
- */
-function zipAPIDataToGeoJSON(geoData, apiReturn) {
-  //Create array of states in the order that the apiReturn came back in
-  apiDataStates = apiReturn.map((datum) => datum.state);
-
-  geoData.features.forEach((geoDatum) => {
-    // Lookup the corresponding entry from the API's data return with the most recent date that matches this entry by state
-    apiDataIndex = apiDataStates.indexOf(geoDatum.properties.NAME);
-
-    let newProps = {
-      GEO_ID: geoDatum.properties.GEO_ID,
-      CENSUSAREA: geoDatum.properties.CENSUSAREA,
-      ...apiReturn[apiDataIndex],
-    };
-
-    geoDatum.properties = newProps;
-  });
-
-  return geoData;
-}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // addition of covid-19 statistics per state
-async function main() {
+async function covid() {
 
   composeStateData().then(stateData => {
     console.log("stateData", stateData);
 
-    let radius_multiplier = 10;
+    let radius_multiplier = 7;
 
     stateData.forEach(state => {
       L.circle([state.latitude, state.longitude], {
@@ -151,7 +78,7 @@ async function main() {
       L.circle([state.latitude, state.longitude], {
         color: "red",
         fillColor: "red",
-        fillOpacity: 0.75,
+        fillOpacity: 1,
         radius: radius_multiplier * state.deaths
       }).addTo(myMap);
     });
@@ -519,7 +446,94 @@ async function composeStateData() {
   return stateData;
 }
 
-main();
+covid();
+
+/**
+ * Takes in geoJson object and appends data returned from the API to each states geoJSON entry
+ * @param {geoJSON OBJECT} geoData The geoJson that is returned from d3.json when querying the geojson file attached
+ * @param {*} apiReturn The return from the API
+ */
+function zipAPIDataToGeoJSON(geoData, apiReturn) {
+  //Create array of states in the order that the apiReturn came back in
+  apiDataStates = apiReturn.map((datum) => datum.state);
+
+  geoData.features.forEach((geoDatum) => {
+    // Lookup the corresponding entry from the API's data return with the most recent date that matches this entry by state
+    apiDataIndex = apiDataStates.indexOf(geoDatum.properties.NAME);
+
+    let newProps = {
+      GEO_ID: geoDatum.properties.GEO_ID,
+      CENSUSAREA: geoDatum.properties.CENSUSAREA,
+      ...apiReturn[apiDataIndex],
+    };
+
+    geoDatum.properties = newProps;
+  });
+
+  return geoData;
+}
+
+//Given a value returns a color code
+function getColor(d) {
+  return d > 100000
+    ? "#008000"
+    : d > 50000
+    ? "#3d9200"
+    : d > 20000
+    ? "#61a400"
+    : d > 10000
+    ? "#81b600"
+    : d > 5000
+    ? "#a1c800"
+    : d > 2000
+    ? "#c0da00"
+    : d > 1000
+    ? "#dfed00"
+    : d > 1
+    ? "#ffff00"
+    : d === "COVID deaths"
+    ? "#e11b22"
+    : d === "COVID cases"
+    ? "#708090"
+    : (d === 0 ? "black" : "black");
+}
+
+//Add the legend
+function addLegend(myMap) {
+  var legend = L.control({ position: "bottomright" });
+
+  legend.onAdd = function (map) {
+    var div = L.DomUtil.create("div", "info legend"),
+      grades = [0, 1, 1000, 2000, 5000, 10000, 20000, 50000, 100000, "COVID deaths", "COVID cases"],
+      labels = [];
+
+    //Add special label for no data (( getColor(grades[0] + 1) ))
+    div.innerHTML +=
+      '<i style="background:' + "black" + '"></i> No Data <br>';
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (let i = 1; i < grades.length - 3; i++) {
+      div.innerHTML +=
+        '<i style="background:' +
+        getColor(grades[i] + 1) +
+        '"></i> ' +
+        grades[i] +
+        (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+
+    for (let i = 8; 7 < i && i < 11; i++) {
+      div.innerHTML +=
+        '<i style="background:' +
+        getColor(grades[i]) +
+        '"></i> ' +
+        grades[i] + "<br><br>";
+    }
+
+    return div;
+    }
+
+  legend.addTo(myMap);
+}
 
 function buildChloropleth(apiReturn) {
   if (geojson) {
@@ -545,7 +559,7 @@ function buildChloropleth(apiReturn) {
         fillOpacity: 0.7,
       };
     }
-
+    
     function onEachFeature(feature, layer) {
       layer.bindPopup(
         `${feature.properties.state}
